b75091ca55e371307cdff76f6c7ab58b6c0fb70b
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-08-31 21:28:15 -0400

    Create thumbnails.js

---
+++ data/thumbnails.js
@@ -0,0 +1,12 @@
+export const thumbnails = [
+    './assets/images/180731-startrekspock-news.jpg',
+    './assets/images/Kirk_and_Enterprise_landing_party.jpg',
+    './assets/images/Several+of+the+original+star+trek+cast+were+veterans+including+_6a713c89e36765f8bd64205ac1bb5502.jpg',
+    './assets/images/Star-Trek-640x478.png',
+    './assets/images/artsculture2-1-2abb593d3dca7aab.jpg',
+    './assets/images/george-takei-sulu.png', 
+    './assets/images/pikeSpock.jpg', 
+    './assets/images/spock-original-wide.jpg',
+    './assets/images/star-trek-day-tos.jpg',
+  ];
+
